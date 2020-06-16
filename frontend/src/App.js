460c7fb3d8d5f605e4fb23bfd870cd938cf6b606
Arthurgallina1 <arthur.gallina@gmail.com>
2020-06-16 18:53:52 -0300

    init

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,20 +2,20 @@ import React from "react";
 import { Router } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
-import history from "./services/history";
+import history from "./utils/history";
 import Routes from "./routes/index";
 import { store, persistor } from "./store";
 
 function App() {
-    return (
-        <Provider store={store}>
-            <PersistGate persistor={persistor}>
-                <Router history={history}>
-                    <Routes />
-                </Router>
-            </PersistGate>
-        </Provider>
-    );
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <Routes />
+        </Router>
+      </PersistGate>
+    </Provider>
+  );
 }
 
 export default App;
